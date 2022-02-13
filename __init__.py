ef8531845351bf69a232f38559477fb44714e982
Bob Gregory <bob.gregory@cazoo.co.uk>
2022-02-13 17:41:05 +0000

    Add poetry, run black

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ from json.decoder import JSONDecoder, JSONDecodeError
 
 import boto3
 
+
 def object_stream(f, decoder=None, bufsize=64 * 1024):
     idx = 0
     data = ""
@@ -37,21 +38,22 @@ def object_stream(f, decoder=None, bufsize=64 * 1024):
 
 def list_files(s3, bucket, prefix):
     files = {
-        'IsTruncated': True,
+        "IsTruncated": True,
     }
 
-    while files['IsTruncated']:
-        if 'NextContinuationToken' in files:
-            files = s3.list_objects_v2(Bucket=bucket, Prefix=prefix, ContinuationToken=files['NextContinuationToken'])
+    while files["IsTruncated"]:
+        if "NextContinuationToken" in files:
+            files = s3.list_objects_v2(
+                Bucket=bucket,
+                Prefix=prefix,
+                ContinuationToken=files["NextContinuationToken"],
+            )
         else:
             files = s3.list_objects_v2(Bucket=bucket, Prefix=prefix)
-        if 'Contents' in files:
-            yield from [f['Key'] for f in files['Contents']]
+        if "Contents" in files:
+            yield from [f["Key"] for f in files["Contents"]]
         else:
             return
-
-
-
 
 
 def sip(*, bucket=None, prefix=None, key=None, s3=None):
@@ -59,15 +61,15 @@ def sip(*, bucket=None, prefix=None, key=None, s3=None):
     if (key and prefix) or not (key or prefix):
         raise ValueError("You must provide either a key or prefix")
 
-    s3 = s3 or boto3.client('s3')
+    s3 = s3 or boto3.client("s3")
 
     if key:
         data = s3.get_object(Key=key, Bucket=bucket)
-        stream = io.TextIOWrapper(data['Body'])
+        stream = io.TextIOWrapper(data["Body"])
         yield from object_stream(stream)
 
     else:
         for key in list_files(s3, bucket, prefix):
             data = s3.get_object(Key=key, Bucket=bucket)
-            stream = io.TextIOWrapper(data['Body'])
+            stream = io.TextIOWrapper(data["Body"])
             yield from object_stream(stream)
--- tests/test_object_sip.py
+++ tests/test_object_sip.py
@@ -10,9 +10,10 @@ from .data import schema
 
 from firehose_sipper import sip
 
-S3 = boto3.client('s3', endpoint_url=os.environ.get("SIPPER_S3_ENDPOINT"))
+S3 = boto3.client("s3", endpoint_url=os.environ.get("SIPPER_S3_ENDPOINT"))
 BUCKET = os.environ.get("SIPPER_BUCKET_NAME")
 PREFIX = str(uuid4())
+
 
 def test_when_the_object_exists():
     num = 5000
@@ -21,7 +22,7 @@ def test_when_the_object_exists():
     data = io.BytesIO()
 
     for entry in schema.create(num):
-        data.write(json.dumps(entry).encode('utf8'))
+        data.write(json.dumps(entry).encode("utf8"))
     data.seek(0)
 
     S3.put_object(Bucket=BUCKET, Key=key, Body=data)
@@ -50,7 +51,7 @@ def test_when_requesting_neither_a_key_nor_prefix():
 def test_when_reading_a_prefix():
     PREFIX = str(uuid4())
     for entry in schema.create(200):
-        data = json.dumps(entry).encode('utf8')
+        data = json.dumps(entry).encode("utf8")
         S3.put_object(Bucket=BUCKET, Key=f"{PREFIX}/{uuid4()}", Body=data)
 
     result = list(sip(bucket=BUCKET, prefix=PREFIX, s3=S3))
